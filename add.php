3457cbb718231067d41b8f752662d744d26248ae
mariamnozadze <mariamnozadze97@yahoo.com>
2022-09-22 22:58:27 +0400

    - Created environment for project  - Created database on myphpadmin  - Connected project to the database  - Added simple layout and styles  - Added Create and Delete functions  - Fixed simple validation

---
+++ add.php
@@ -0,0 +1,108 @@
+<?php
+
+//Create connection to the database
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+
+//If connection isn't successful; so when there is an error, throw an exception
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+/* -------check what do i get from submit-------
+echo '<pre?>';
+var_dump($_GET);
+echo '</pre>';
+*/
+
+$errors = [];
+
+$sku = '';
+$title = '';
+$price = '';
+$size_mb = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $sku = $_POST['sku'];
+    $title = $_POST['title'];
+    $price = $_POST['price'];
+    $size_mb = $_POST['size_mb'];
+
+    if (!$sku) {
+        $errors[] = 'Product SKU is required';
+    }
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (!$size_mb) {
+        $errors[] = 'Product Size is required';
+    }
+
+    if (empty($errors)) {
+        $statement = $pdo->prepare("INSERT INTO products (sku, title, price, size_mb)
+                VALUES (:sku, :title,  :price, :size_mb)");
+        $statement->bindValue(':sku', $sku);
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':size_mb', $size_mb);
+
+        $statement->execute();
+        header('Location: index.php');
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="app.css">
+    <title>Product CRUD</title>
+</head>
+
+<body>
+    <div class="header align-content-start">
+        <h1>Add New Product</h1>
+    </div>
+
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <form method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+            <label>SKU</label>
+            <input type="text" name="sku" class="form-control" value="<?php echo $sku ?>">
+        </div>
+        <div class="form-group">
+            <label>Title</label>
+            <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        </div>
+        <div class="form-group">
+            <label>Price</label>
+            <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+        </div>
+        <div class="form-group">
+            <label>Size</label>
+            <input type="text" name="size_mb" class="form-control" value="<?php echo $size_mb ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+//Create connection to the database
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+
+//If connection isn't successful; so when there is an error, throw an exception
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//Select and display products from mysql
+$statement = $pdo->prepare('SELECT * FROM products');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// echo '<pre>';
+// var_dump($products);
+// echo '</pre>';
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./app.css">
+    <title>Product CRUD</title>
+</head>
+
+<body>
+    <div class="header justify-content-around align-content-center">
+        <h1>Product List</h1>
+
+        <p>
+            <a href="add.php" class="btn btn-success">Add Product</a>
+        </p>
+    </div>
+
+    <div class="container">
+        <?php foreach ($products as $product) : ?>
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-text">
+                        <?php echo $product['sku'] ?>
+                    </div>
+                    <div class="card-text">
+                        <?php echo $product['title'] ?>
+                    </div>
+                    <div class="card-text">
+                        <?php echo $product['price'] . ' ' . '$' ?>
+                    </div>
+                    <div class="card-text">
+                        <?php echo $product['size_mb'] . ' ' . 'MB'  ?>
+                    </div>
+                    <form method="post" action="delete.php" style="display: inline-block">
+                        <input type="hidden" name="id" value="<?php echo $product['id'] ?>" />
+                        <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+</body>
+
+</html>
